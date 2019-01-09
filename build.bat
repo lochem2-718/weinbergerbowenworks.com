f951663b9ad4f179b3a8ccefc386dc24c9b2b17f
Jared Weinberger <jared.j.weinberger@gmail.com>
2019-01-08 21:44:09 -0700

    added gulp, made mobile menu

--- build.bat
+++ build.bat
@@ -1,1 +1,16 @@
-move files/bowenwork-brochure.pdf prod/files/bowenwork-brochure.pdf
+copy files\press-release.pdf prod\files\press-release.pdf
+copy files\bowenwork-brochure.pdf prod\files\bowenwork-brochure.pdf
+
+copy images\main-image-mobile.jpg prod\images\main-image-mobile.jpg
+copy images\main-image.jpg prod\images\main-image.jpg
+copy images\yolanda-image.jpeg prod\images\yolanda-image.jpeg
+
+copy open-sans\OpenSans-Regular.ttf prod\open-sans\OpenSans-Regular.ttf
+
+copy bootstrap\css\bootstrap.min.css prod\bootstrap\css\bootstrap.min.css
+minify presentation.css -o prod\presentation.css
+minify presentation.css -o presentation.css
+
+htmlmin index.html -o prod\index.html
+htmlmin contact.html -o prod\contact.html
+htmlmin appointment-info.html -o prod\appointment-info.html
